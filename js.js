701873f78303ad0a35061908fe3dd45e7e6ea38c
Jose L. Gazano <josegazano7@outlook.com>
2021-12-05 15:23:36 -0300

    Se agrego una funcion saludar JLG

---
+++ js.js
@@ -0,0 +1,7 @@
+
+function saludar() {
+    document.getElementById("titulo").innerHTML = "Buenos dias Estudiantes!";
+}
+document.getElementById("saludo1").onclick = function () {
+    saludar();
+}
